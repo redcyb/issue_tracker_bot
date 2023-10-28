cfe180cfb99859a7df499f43a6836e9e193650b2
Redcyb <redcyb@gmail.com>
2023-10-28 22:05:20 +0200

    Predefined options for problems and solutions (#16)
    * add menu commands to callback actions.
    refactor callbacks
    
    * refactor callbacks
    
    * add options to menu

--- issue_tracker_bot/services/__init__.py
+++ issue_tracker_bot/services/__init__.py
@@ -1,2 +1,2 @@
-from issue_tracker_bot.services.actions import Actions
+from issue_tracker_bot.services.actions import Actions, MenuCommandStates
 from issue_tracker_bot.services.gcloud.service import GCloudService
--- issue_tracker_bot/services/actions.py
+++ issue_tracker_bot/services/actions.py
@@ -4,4 +4,10 @@ from enum import Enum
 class Actions(Enum):
     PROBLEM = "проблема"
     SOLUTION = "рішення"
-    REPORT = "статус"
+    STATUS = "статус"
+
+
+class MenuCommandStates(Enum):
+    INITIAL_ACTION_SELECTED = "cmd_ias"
+    DEVICE_SELECTED_FOR_ACTION = "cmd_dsfa"
+    OPTION_SELECTED_FOR_ACTION = "cmd_osfa"
