179e5163671f91d9c019eab258e80305a6d0e538
omar <redcyb@gmail.com>
2024-01-06 18:11:18 +0100

    add snapshotter to export data to gcloud

--- issue_tracker_bot/services/export_data.py
+++ issue_tracker_bot/services/export_data.py
@@ -24,9 +24,7 @@ def export_reports_to_gdoc():
 
     data = [header, *[list(d.values()) for d in data]]
 
-    sheet_name = str(datetime.now().date())
-
-    result = gc.export_records(sheet_name, data)
+    result = gc.export_records(str(datetime.now().date()), data)
 
     return result
 
